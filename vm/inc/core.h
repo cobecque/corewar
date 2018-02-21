e7afc9ef7c8a8c52c3b5cc3f2de60c422566e388
Corentin BECQUEY <cobecque@e1r8p19.42.fr>
2018-02-21 13:48:02 +0100

    Fefe

--- vm/inc/core.h
+++ vm/inc/core.h
@@ -6,53 +6,56 @@
 /*   By: rostroh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/12 10:40:59 by rostroh           #+#    #+#             */
-/*   Updated: 2018/02/14 11:03:50 by rostroh          ###   ########.fr       */
+/*   Updated: 2018/02/20 13:17:17 by cobecque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include "ft_printf.h"
+#ifndef CORE_H
+# define CORE_H
 
-#define IND_SIZE			2
-#define REG_SIZE			4
-#define DIR_SIZE			REG_SIZE
+# include "libft.h"
+# include "ft_printf.h"
 
-#define REG_CODE			1
-#define DIR_CODE			2
-#define IND_CODE			3
+# define IND_SIZE			2
+# define REG_SIZE			4
+# define DIR_SIZE			REG_SIZE
 
-#define MAX_ARGS_NUMBERS	4
-#define MAX_PLAYERS			4
-#define MEM_SIZE			(4 * 1024)
-#define IDX_MOD				(MEM_SIZE / 8)
-#define CHAMP_MAX_SIZE		(MEM_SIZE / 6)
+# define REG_CODE			1
+# define DIR_CODE			2
+# define IND_CODE			3
 
-#define COMMENT_CHAR		'#'
-#define LABEL_CHAR			':'
-#define DIRECT_CHAR			'%'
-#define SEPARATOR_CHAR		','
+# define MAX_ARGS_NUMBERS	4
+# define MAX_PLAYERS			4
+# define MEM_SIZE			(4 * 1024)
+# define IDX_MOD				(MEM_SIZE / 8)
+# define CHAMP_MAX_SIZE		(MEM_SIZE / 6)
 
-#define LABEL_CHARS			"abcdefghijklmnopqrstuvwxyz_0123456789"
+# define COMMENT_CHAR		'#'
+# define LABEL_CHAR			':'
+# define DIRECT_CHAR			'%'
+# define SEPARATOR_CHAR		','
 
-#define NAME_CMD_STRING		".name"
-#define COMMENT_CMD_STRIG	".comment"
+# define LABEL_CHARS			"abcdefghijklmnopqrstuvwxyz_0123456789"
 
-#define REG_NUMBER			16
+# define NAME_CMD_STRING		".name"
+# define COMMENT_CMD_STRIG	".comment"
 
-#define CYCLE_TO_DIE		1536
+# define REG_NUMBER			16
 
-#define CYCLE_DELTA			50
-#define NBR_LIVE			21
-#define MAX_CHECK			10
+# define CYCLE_TO_DIE		1536
 
-#define T_REG				1
-#define T_DIR				2
-#define T_IND				4
-#define T_LAB				8
+# define CYCLE_DELTA			50
+# define NBR_LIVE			21
+# define MAX_CHECK			10
 
-#define PROG_NAME_LENGTH	(128)
-#define COMMENT_LENGTH		(2048)
-#define COREWAR_EXEC_MAGIC	0xea83f3
+# define T_REG				1
+# define T_DIR				2
+# define T_IND				4
+# define T_LAB				8
+
+# define PROG_NAME_LENGTH	(128)
+# define COMMENT_LENGTH		(2048)
+# define COREWAR_EXEC_MAGIC	0xea83f3
 
 typedef struct				s_cycle
 {
@@ -95,6 +98,7 @@ typedef struct				s_process
 	char					*ins;
 	int						carry;
 	int						line;
+	int						ocp;
 	int						live;
 	int						last_live[2];
 	int						seek;
@@ -155,53 +159,69 @@ typedef struct				s_vm
 	t_champ					play[MAX_PLAYERS];
 }							t_vm;
 
-void						dump(char *ptr);
-int							check_r(t_inf inf);
-void						ft_fork(t_inf inf, t_process *pros, t_vm vm);
-void						ft_lfork(t_inf inf, t_process *pros, t_vm vm);
+t_process					*calcul_val(t_process *c, int *y, unsigned int *a,
+		int *p);
+t_process					*kill_them_all(t_process *p, t_vm v, int cy, int c);
+t_process					*gestion_process(t_process *pro, int cy, t_vm vm);
+t_process					*call_tree(t_inf truc, t_process *pros, t_vm vm);
+t_process					*ocp_invalid(t_process *c, int *bol, int *len);
 t_process					*dup_pros(t_process *src, t_inf inf, t_vm vm);
-char						*get_hexa(int val);
+t_process					*find_val(t_process *cpy, int *adv, int *len);
+t_process					*start_gestion(t_process *c, t_vm vm, int cy);
+t_process					*adv_printf(t_process *cpy, int len, int nb);
+t_process					*add_new_process(t_process *src, int nb);
+t_process					*move_pc(t_process *cpy, int len);
+
+t_cycle						init_cycle(void);
+
 t_inf						nb_oct(t_inf srt, int line, int ocp);
+t_inf						nb_oct(t_inf srt, int line, int ocp);
+t_inf						add_elem(int info, int opc);
+
+t_ver						get_verbose(int nu);
+
+t_pam						get_option(int argc, char **argv);
+
 t_vm						fill_champ(int *fd);
-void						reg_write(t_process *pros, unsigned int val, int reg, int size);
-void						vm_stuff(t_vm data);
+
+void						reg_write(t_process *pros, unsigned int val,
+		int reg, int size);
+void						message_champ(int nb, int winner, t_champ champ);
+void						ft_lfork(t_inf inf, t_process *pros, t_vm vm);
+void						ft_lldi(t_inf inf, t_process *pros, t_vm vm);
+void						ft_fork(t_inf inf, t_process *pros, t_vm vm);
+void						ft_live(t_inf info, t_process *pro, t_vm vm);
+void						ft_zjmp(t_inf inf, t_process *pros, t_vm vm);
 void						ft_add(t_inf inf, t_process *pros, t_vm vm);
 void						ft_aff(t_inf inf, t_process *pros, t_vm vm);
-void						ft_and(t_inf, t_process *pros, t_vm vm);
-void						ft_ld(t_inf inf, t_process *pros, t_vm vm);
 void						ft_ldi(t_inf inf, t_process *pros, t_vm vm);
 void						ft_lld(t_inf inf, t_process *pros, t_vm vm);
-void						ft_lldi(t_inf inf, t_process *pros, t_vm vm);
-void						ft_or(t_inf inf, t_process *pros, t_vm vm);
-void						ft_st(t_inf inf, t_process *pros, t_vm vm);
 void						ft_sti(t_inf inf, t_process *pros, t_vm vm);
 void						ft_sub(t_inf inf, t_process *pros, t_vm vm);
 void						ft_xor(t_inf inf, t_process *pros, t_vm vm);
-void						ft_zjmp(t_inf, t_process *pros, t_vm vm);
-int							have_ocp(int line);
-int							get_line(int opc);
-int							get_ocp(void *addr);
+void						ft_and(t_inf inf, t_process *pros, t_vm vm);
+void						ft_or(t_inf inf, t_process *pros, t_vm vm);
+void						ft_st(t_inf inf, t_process *pros, t_vm vm);
+void						ft_ld(t_inf inf, t_process *pros, t_vm vm);
 void						cpy_reg(int *tab, int **res);
-t_inf						add_elem(int info, int opc);
-t_process					*gestion_process(t_process *pro, int cy, t_vm vm);
-t_process					*call_tree(t_inf truc, t_process *pros, t_vm vm);
+void						vm_stuff(t_vm data);
+void						dump(char *ptr);
+void						message(int nb);
+
+char						*get_hexa(int val);
+
 int							cycle_gestion(t_vm vm, t_process *pro, t_cycle cy);
-void						ft_live(t_inf info, t_process *pro, t_vm vm);
+int							type_param(int ocp, int pos, int line, int *res);
 int							ocp_valid(int line, int ocp);
 int							adv_value(int line, int ocp);
-t_ver						get_verbose(int nu);
-void						message(int nb);
-t_pam						get_option(int argc, char **argv);
-int							check_magic(int fd);
 int							string_is_digit(char *str);
-t_inf						nb_oct(t_inf srt, int line, int ocp);
-int							type_param(int ocp, int pos, int line, int *res);
-void						message_champ(int nb, int winner, t_champ champ);
-t_process					*add_new_process(t_process *src, int nb);
-t_process					*kill_them_all(t_process *p, t_vm v, int cy, int c);
 int							count_live(t_process *pro);
 int							winner(t_process *pro);
-t_cycle						init_cycle(void);
+int							get_ocp(void *addr);
+int							check_magic(int fd);
+int							have_ocp(int line);
+int							check_r(t_inf inf);
+int							get_line(int opc);
 
 static void					(*g_instructab[17])(t_inf, t_process *pros, t_vm vm) =
 {
@@ -209,3 +229,5 @@ static void					(*g_instructab[17])(t_inf, t_process *pros, t_vm vm) =
 	&ft_zjmp, &ft_ldi, &ft_sti, &ft_fork, &ft_lld, &ft_lldi, &ft_lfork, 
 	&ft_aff, NULL
 };
+
+#endif
--- vm/src/process.c
+++ vm/src/process.c
@@ -6,41 +6,105 @@
 /*   By: rostroh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/14 08:00:17 by rostroh           #+#    #+#             */
-/*   Updated: 2018/02/14 09:21:15 by rostroh          ###   ########.fr       */
+/*   Updated: 2018/02/20 13:17:46 by cobecque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "core.h"
 
+t_process	*find_val(t_process *cpy, int *adv, int *len)
+{
+	unsigned int	a;
+	int				yolo;
+	int				p;
+	int				i;
+
+	yolo = 0;
+	i = 0;
+	while (i < g_op_tab[cpy->line].nb_arg)
+	{
+		p = 0;
+		a = 0;
+		(*adv) = (*adv) + cpy->inf.l[i];
+		while (p < cpy->inf.l[i])
+		{
+			if ((cpy->pc + yolo) >= cpy->inf.min_addr + MEM_SIZE)
+				(*len) = yolo;
+			cpy = calcul_val(cpy, &yolo, &a, &p);
+			p++;
+			yolo++;
+		}
+		cpy->inf.val[i] = a;
+		i++;
+	}
+	(*len) = (*len) + yolo;
+	return (cpy);
+}
+
+t_process	*calcul_val(t_process *cpy, int *yolo, unsigned int *a, int *p)
+{
+	if ((cpy->pc + (*yolo)) >= cpy->inf.min_addr + MEM_SIZE)
+	{
+		*yolo = 0;
+		cpy->pc = cpy->inf.min_addr;
+	}
+	if (*p == 0)
+		*a = *(cpy->pc + (*yolo));
+	else
+		*a = ((*a) << 8) | ((unsigned int)(*(cpy->pc + (*yolo)) << 24) >> 24);
+	return (cpy);
+}
+
+t_process	*start_gestion(t_process *cpy, t_vm vm, int cycle)
+{
+	cpy->inf.min_addr = vm.addr;
+	if (cpy->pc >= (cpy->inf.min_addr + MEM_SIZE))
+		cpy->pc = cpy->inf.min_addr;
+	if (cpy->pc < cpy->inf.min_addr)
+	{
+		if (cpy->pc < 0)
+			cpy->pc = (char *)((long)cpy->pc + (cpy->inf.min_addr +
+						MEM_SIZE));
+		else
+			cpy->pc = (char *)((cpy->inf.min_addr + MEM_SIZE) -
+					(long)cpy->pc);
+	}
+	if (cpy->seek == 0)
+		cpy->line = get_line(*(cpy->pc));
+	if (cpy->line != -1 && cpy->start_cycle == -1)
+	{
+		cpy->seek = 1;
+		if (cycle != 0)
+			cpy->start_cycle = cycle - 1;
+		else
+			cpy->start_cycle = cycle;
+	}
+	return (cpy);
+}
+
+t_process	*init_inf(t_process *cpy, t_vm vm, int *nb, int *bol)
+{
+	*nb = 0;
+	*bol = 0;
+	cpy->ocp = 0;
+	cpy->ins = cpy->pc;
+	cpy->pc++;
+	if (cpy->pc >= cpy->inf.min_addr + MEM_SIZE)
+		cpy->pc = cpy->inf.min_addr;
+	cpy->inf = add_elem(cpy->line, *(cpy->pc));
+	cpy->inf.min_addr = vm.addr;
+	return (cpy);
+}
+
 t_process	*gestion_process(t_process *pro, int cycle, t_vm vm)
 {
 	t_process		*cpy;
-	t_inf			inf;
 	int				nb;
-	int				i;
 	int				bol;
-	int				bol2;
-	char			*ret;
-	unsigned int	a;
-	unsigned int	b;
-	int				p;
 	int				adv;
-	int				ocp;
-	int				yolo;
 	int				len;
 
-	len = 0;
-	a = 0;
-	bol = 0;
-	bol2 = 0;
-	vm.error = 1;
 	cpy = pro;
-	inf.min_addr = vm.addr;
-	ret = cpy->pc;
-	nb = 0;
-	p = 0;
-	ocp = 0;
-	adv = 0;
 	if (vm.arg.ver_num.cy == 1)
 		ft_printf("It is now cycle %d\n", cycle);
 	while (cpy->next != NULL)
@@ -49,185 +113,65 @@ t_process	*gestion_process(t_process *pro, int cycle, t_vm vm)
 	{
 		if (cpy->live != -1)
 		{
-			inf.min_addr = vm.addr;
-			if (cpy->pc >= (inf.min_addr + MEM_SIZE))
-				cpy->pc = inf.min_addr;
-			if (cpy->pc < inf.min_addr)
-			{
-				if (cpy->pc < 0)
-					cpy->pc = (char *)((long)cpy->pc + (inf.min_addr +
-								MEM_SIZE));
-				else
-					cpy->pc = (char *)((inf.min_addr + MEM_SIZE) -
-							(long)cpy->pc);
-			}
-			if (cpy->seek == 0)
-				cpy->line = get_line(*(cpy->pc));
-			i = 0;
-			if (cpy->line != -1 && cpy->start_cycle == -1)
-			{
-				cpy->seek = 1;
-				if (cycle != 0)
-					cpy->start_cycle = cycle - 1;
-				else
-					cpy->start_cycle = cycle;
-			}
+			cpy = start_gestion(cpy, vm, cycle);
 			if (cpy->start_cycle + g_op_tab[cpy->line].cycle == cycle &&
 					cpy->line <= 17 && cpy->line >= 0)
 			{
 				if (cpy->seek == 1)
 				{
-					yolo = 0;
-					adv = 0;
-					bol = 0;
-					ocp = 0;
-					nb = 0;
 					if (!cpy->pc)
 						break ;
-					cpy->ins = cpy->pc;
-					cpy->pc++;
-					if (cpy->pc >= inf.min_addr + MEM_SIZE)
-						cpy->pc = inf.min_addr;
-					cpy->inf = add_elem(cpy->line, *(cpy->pc));
-					cpy->inf.min_addr = vm.addr;
+					cpy = init_inf(cpy, vm, &nb, &bol);
 					if (have_ocp(cpy->line) == 0)
 					{
 						nb = 1;
-						ocp = *(cpy->pc);
-						bol2 = cpy->line;
-						if (ocp_valid(bol2, ocp) == 0)
+						cpy->ocp = *(cpy->pc);
+						if (ocp_valid(cpy->line, cpy->ocp) == 0)
 							bol = -1;
 						cpy->pc++;
-						if (cpy->pc >= inf.min_addr + MEM_SIZE)
-							cpy->pc = inf.min_addr;
-						if (bol == -1)
-						{
-							adv = adv_value(cpy->line, ocp);
-							if (vm.arg.ver_num.pc == 1)
-								ft_printf("ADV %d (0x%.4x -> 0x%.4x) %s %s",
-										adv + 2, cpy->ins - cpy->inf.min_addr,
-										cpy->ins - cpy->inf.min_addr + adv + 2,
-										get_hexa(*cpy->ins), get_hexa(ocp));
-							yolo = 0;
-							while (yolo < adv)
-							{
-								if (vm.arg.ver_num.pc == 1)
-									ft_printf(" %s", get_hexa(*cpy->pc));
-								cpy->pc++;
-								if (cpy->pc == inf.min_addr + MEM_SIZE)
-									cpy->pc = inf.min_addr;
-								yolo++;
-							}
-							if (vm.arg.ver_num.pc == 1)
-								ft_printf(" \n");
-							cpy->line = -1;
-						}
+						if (cpy->pc >= cpy->inf.min_addr + MEM_SIZE)
+							cpy->pc = cpy->inf.min_addr;
+						if (vm.arg.ver_num.pc == 1 && bol == -1)
+							bol = -2;
+						cpy = ocp_invalid(cpy, &bol, &len);
 					}
 					if (bol == 0)
 					{
 						if (!(cpy->inf.val = (int *)malloc(sizeof(int) * 3)))
 							return (NULL);
-						i = 0;
 						len = 0;
 						adv = 0;
-						yolo = 0;
-						bol2 = 0;
-						while (i < g_op_tab[cpy->line].nb_arg)
-						{
-							p = 0;
-							a = 0;
-							adv += cpy->inf.l[i];
-							while (p < cpy->inf.l[i])
-							{
-								if ((cpy->pc + len) >= inf.min_addr + MEM_SIZE)
-								{
-									yolo = len;
-									len = 0;
-									cpy->pc = inf.min_addr;
-								}
-								if (p == 0)
-									a = *(cpy->pc + len);
-								else
-								{
-									b = *(cpy->pc + len) << 24;
-									a = (a << 8) | (b >> 24);
-								}
-								if ((cpy->pc + len) >= inf.min_addr + MEM_SIZE)
-								{
-									yolo = len;
-									len = 0;
-									cpy->pc = inf.min_addr;
-								}
-								p++;
-								len++;
-							}
-							cpy->inf.val[i] = a;
-							i++;
-						}
+						bol = 1;
+						find_val(cpy, &adv, &len);
 						cpy->seek = 2;
 						if (nb == 1)
 						{
-							bol2 = 2;
-							len = yolo + len + 2;
+							bol = 2;
+							len += 2;
 						}
 						else
-						{
-							bol2 = 1;
-							len = yolo + len + 1;
-						}
+							len += 1;
 						g_instructab[cpy->line](cpy->inf, cpy, vm);
 						cpy->pc = cpy->ins;
 						if (vm.arg.ver_num.pc == 1 && (cpy->line != 8 ||
 									cpy->carry == 0))
 						{
-							p = 0;
+							adv++;
 							if (nb == 1)
-								adv += 2;
-							else
 								adv++;
-							i = 0;
 							ft_printf("ADV %d (0x%.4x -> 0x%.4x) %s", adv,
-									cpy->ins - inf.min_addr, cpy->ins -
-									inf.min_addr + adv, get_hexa(*cpy->ins));
+									cpy->ins - cpy->inf.min_addr, cpy->ins -
+									cpy->inf.min_addr + adv,
+									get_hexa(*cpy->ins));
 							if (nb == 1)
 								ft_printf(" %s", get_hexa(*(cpy->ins + 1)));
 							nb++;
-							while (i < len - bol2)
-							{
-								if ((cpy->ins + p + nb) >= inf.min_addr +
-										MEM_SIZE)
-								{
-									nb = 0;
-									p = 0;
-									cpy->ins = inf.min_addr;
-								}
-								ft_printf(" %s", get_hexa(*(cpy->ins + p +
-												nb)));
-								p++;
-								i++;
-								if ((cpy->ins + p) >= inf.min_addr + MEM_SIZE)
-								{
-									p = 0;
-									nb = 0;
-									cpy->ins = inf.min_addr;
-								}
-							}
+							cpy = adv_printf(cpy, len - bol, nb);
 							if (vm.arg.ver_num.pc == 1)
 								ft_printf(" \n");
 						}
-						i = 0;
-						if (cpy->line != 8 || cpy->carry == 0)
-						{
-							while (i < len)
-							{
-								if (cpy->pc > inf.min_addr + MEM_SIZE)
-									cpy->pc = (inf.min_addr + 1);
-								cpy->pc++;
-								i++;
-							}
-							if (cpy->pc > inf.min_addr + MEM_SIZE)
-								cpy->pc = inf.min_addr + 1;
-						}
+						cpy = move_pc(cpy, len);
+						free(cpy->inf.val);
 					}
 					cpy->start_cycle = -1;
 					cpy->seek = 0;
@@ -235,12 +179,12 @@ t_process	*gestion_process(t_process *pro, int cycle, t_vm vm)
 			}
 			else if (cpy->line == -1)
 			{
-				if (cpy->pc >= inf.min_addr + MEM_SIZE)
-					cpy->pc = inf.min_addr;
+				if (cpy->pc >= cpy->inf.min_addr + MEM_SIZE)
+					cpy->pc = cpy->inf.min_addr;
 				cpy->pc++;
 			}
-			if (cpy->pc >= inf.min_addr + MEM_SIZE)
-				cpy->pc = inf.min_addr;
+			if (cpy->pc >= cpy->inf.min_addr + MEM_SIZE)
+				cpy->pc = cpy->inf.min_addr;
 		}
 		cpy = cpy->pre;
 	}

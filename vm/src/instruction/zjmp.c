2806c9a9ba91eef59d13cac24292c9edda342284
Corentin BECQUEY <cobecque@e1r11p22.42.fr>
2018-03-01 23:11:01 +0100

    fefe

--- vm/src/instruction/zjmp.c
+++ vm/src/instruction/zjmp.c
@@ -6,7 +6,7 @@
 /*   By: rostroh <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 13:26:36 by rostroh           #+#    #+#             */
-/*   Updated: 2018/02/26 18:38:14 by rostroh          ###   ########.fr       */
+/*   Updated: 2018/03/01 21:43:58 by cobecque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void		ft_zjmp(t_inf inf, t_process *pros, t_vm *vm)
 {
+	int		value;
+
 	if (vm->arg.ver_num.op == 1 && pros->carry == 0)
 		ft_printf("P%5d | zjmp %d FAILED\n", pros->number, inf.val[0]);
 	if (pros->carry == 1)
@@ -25,10 +27,11 @@ void		ft_zjmp(t_inf inf, t_process *pros, t_vm *vm)
 			pros->ins = (pros->ins - (inf.min_addr + MEM_SIZE)) + inf.min_addr;
 		if (pros->ins < inf.min_addr)
 		{
-			if (pros->ins >= 0)
+			value = (int)pros->ins;
+			if (value >= 0)
 				pros->ins = (inf.min_addr + MEM_SIZE - 1 - (int)pros->ins);
 			else
-				pros->ins = (int)pros->ins + (inf.min_addr + MEM_SIZE - 1);
+				pros->ins = inf.min_addr + (MEM_SIZE - (int)inf.min_addr + (int)pros->ins);
 		}
 		pros->pc = pros->ins;
 	}

7672b46205495d43beb2aa66e1c9981adde7e681
Corentin BECQUEY <cobecque@e1r3p21.42.fr>
2018-03-08 07:28:30 +0100

    fef

--- asm/src/main.c
+++ asm/src/main.c
@@ -5,144 +5,102 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cobecque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/20 15:23:10 by cobecque          #+#    #+#             */
-/*   Updated: 2017/11/07 19:07:42 by cobecque         ###   ########.fr       */
+/*   Created: 2018/02/12 00:55:32 by cobecque          #+#    #+#             */
+/*   Updated: 2018/03/08 07:27:06 by cobecque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "corewar.h"
+#include "asm.h"
 
-int		main(int argc, char **argv)
+void			ft_putchar_fd_size(char c, char c2, char c3, int fd)
 {
-	t_file		*file;
-	int			fd;
-	char		*line;
-	char		*name;
-	char		*comment;
-	int			i;
-	int			j;
-	header_t	header;
-
-	header.prog_name[0] = '\0';
-	header.comment[0] = '\0';
-	header.prog_size = 0;
-	header.magic = 0xea83f3;
-	file = NULL;
-	name = NULL;
-	comment = NULL;
-	if (argc > 0)
-	{
-		fd = open(argv[1], O_RDONLY);
-		while (get_next_line(fd, &line) > 0)
-		{
-			if (ft_strncmp(line, ".name", 5) == 0)
-				name = ft_strsub(line, 6, ft_strlen(line) - 6);
-			else if (ft_strncmp(line, ".comment", 8) == 0)
-				comment = ft_strsub(line, 9, ft_strlen(line) - 9);
-			else if (ft_strncmp(line, "#", 1) == 0)
-				;
-			else
-				file = init_file(file, line);
-			free(line);
-		}
-		close(fd);
-	}
-	j = 0;
-	i = 1;
-	while (i < PROG_NAME_LENGTH + 1 && i < (int)ft_strlen(name) - 1)
-	{
-		header.prog_name[j] = name[i];
-		i++;
-		j++;
-	}
-	i = 1;
-	j = 0;
-	while (i < COMMENT_LENGTH + 1 && i < (int)ft_strlen(comment) - 1)
-	{
-		header.comment[j] = comment[i];
-		i++;
-		j++;
-	}
-	name = get_name(argv[1]);
-	fd = open(name, O_CREAT | O_WRONLY);
-	chmod(name, S_IRUSR | S_IWUSR);
-	ft_asm(file, fd, header);
-	close(fd);
-	return (0);
+	ft_putchar_fd(c, fd);
+	ft_putchar_fd(c2, fd);
+	ft_putchar_fd(c3, fd);
 }
 
-void	ft_asm(t_file *file, int fd, header_t header)
+void			clear_file(t_file file)
 {
-	t_file		*tmp;
+	t_inst	*tmp;
+	t_inst	*keep;
 
-	tmp = file;
-	tmp = ft_encodage(tmp, fd, header);
-}
-
-t_file	*ft_encodage(t_file *file, int fd, header_t header)
-{
-	t_file	*tmp;
-
-	tmp = file;
-	tmp = ft_label(tmp);
-	tmp = ft_instruction(tmp);
-	tmp = ft_ocp(tmp);
-	tmp = ft_parametre(tmp);
-	tmp = file_param(tmp);
-	tmp = ft_spec_param(tmp);
-	tmp = fill_binair(tmp, fd, header);
-	return (tmp);
-}
-
-t_file	*ft_label(t_file *file)
-{
-	t_file	*tmp;
-	int		i;
-
-	tmp = file;
+	tmp = file.ins;
 	while (tmp != NULL)
 	{
-		i = 0;
-		if (tmp->line != NULL)
-		{
-			while (tmp->line[i] != '\0' && tmp->line[i] != ':')
-				i++;
-			if (tmp->line[i] == ':')
-			{
-				tmp->label = ft_strsub(tmp->line, 0, i);
-				if (ft_is_label(tmp->label) == 1)
-					tmp->label = NULL;
-			}
-		}
+		keep = tmp;
+		if (tmp->label != NULL)
+			free(tmp->label);
+		if (tmp->label1 != NULL)
+			free(tmp->label1);
+		if (tmp->label2 != NULL)
+			free(tmp->label2);
+		if (tmp->label3 != NULL)
+			free(tmp->label3);
 		tmp = tmp->next;
+		free(keep);
 	}
-	return (file);
+	if (file.name != NULL)
+		free(file.name);
+	if (file.comment != NULL)
+		free(file.comment);
 }
 
-int		ft_is_label(char *str)
+int				main(int argc, char **argv)
 {
-	char	*label;
-	int		i;
-	int		j;
-	int		bol;
+	char		*line;
+	t_file		file;
+	int			fd;
 
-	label = ft_strdup(LABEL_CHARS);
-	i = 0;
-	j = 0;
-	while (str[i] != '\0')
+	line = NULL;
+	if (argc == 2)
 	{
-		j = 0;
-		bol = 0;
-		while (label[j] != '\0' && bol == 0)
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+			return (error_open(fd));
+		file = init_file();
+		while (get_next_line(fd, &line) > 0)
 		{
-			if (str[i] == label[j])
-				bol++;
-			else
-				j++;
+			file = fill_file(file, line);
+			free(line);
 		}
-		if (bol == 0)
-			return (1);
-		i++;
+		if (check_error(file) == -1)
+			return (0);
+		file.ins = find_label(file.ins);
+		create_file(argv[1], file);
+		if (close(fd) == -1)
+			ft_printf("Error : Close file\n");
+		clear_file(file);
 	}
 	return (0);
+}
+
+int				error_open(int fd)
+{
+	if (fd == -1)
+		ft_putstr("Error : Open file\n");
+	return (0);
+}
+
+void			create_file(char *argv, t_file file)
+{
+	t_header	h;
+	char		*name;
+	int			fd;
+
+	h.size = size_champ(file);
+	if (!(name = (char *)malloc(sizeof(char) * ft_strlen(argv) + 3)))
+		exit(-1);
+	name = ft_strcpy(name, argv);
+	name[ft_strlen(argv) - 2] = '\0';
+	name = ft_strcat(name, ".cor");
+	fd = open(name, O_CREAT | O_WRONLY | O_TRUNC);
+	if (fd == -1)
+		ft_printf("open error\n");
+	chmod(name, S_IRUSR | S_IWUSR);
+	fill_magic(fd, h);
+	fill_champ(fd, file, h);
+	write_binary(file, fd);
+	free(name);
+	if (close(fd) == -1)
+		ft_printf("close error\n");
 }
